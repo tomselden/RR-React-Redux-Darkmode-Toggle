2d618df373df06e3aaa7baee1056c9acf5287858
Thomas Selden <thomasselden42@gmail.com>
2022-02-16 15:34:12 -0500

    images render upon page re-render and added middleware logger

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
+import "./App.css";
+import { useSelector, useDispatch, connect } from "react-redux";
+import {
+  clearData,
+  fetchData,
+  incrementId,
+  decrementId,
+  inputId,
+} from "./features/dataSlice";
+import { useEffect } from "react";
 
 function App() {
-  // your logic goes here!
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.data);
+
+  const renderImg = () => {
+    if (data.apiData) {
+      return (
+        <img
+          style={{ width: "100vw" }}
+          src={data.apiData.primaryImage}
+          alt={data.apiData.title}
+        />
+      );
+    } else {
+      return <p>image here</p>;
+    }
+  };
+
+  // this was erroring when it was props.objectId(error: props is undefined) but it works now that it is data.objectId 
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [data.objectId, dispatch]);
 
   return (
     <div className="App">
       <div>
-        <button onClick={() => {}}>Trigger Thunk</button>
-        <button onClick={() => {}}>Clear</button>
-        <button onClick={() => {}}>Next</button>
-        <button onClick={() => {}}>Back</button>
+        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
+        <button onClick={() => dispatch(clearData())}>Clear</button>
+        <button onClick={() => dispatch(incrementId())}>Next</button>
+        <button onClick={() => dispatch(decrementId())}>Back</button>
       </div>
-      <input onChange={(e) => { }} />
+      <input onChange={(e) => dispatch(inputId())} />
       <div>
-        {/* Once you have plugged everything in, render the image here! */}
+        {data.objectId}
+        {renderImg()}
       </div>
     </div>
   );
 }
 
-export default App;
+// passing state in for Connect, this method is called mapping state to props
+const mapStateToProps = (state) => ({
+  objectId: state.data.objectId,
+});
+
+export default connect(mapStateToProps)(App);
